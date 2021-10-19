d20954df04697ac83143018a9dfb8df535658a07
Taylor Lomax <taylor@wilsonlomax.com>
2021-10-18 20:30:17 -0500

    Login Successful

--- cypress/integration/modules/Assessments/assessment.spec.js
+++ cypress/integration/modules/Assessments/assessment.spec.js
@@ -1,3 +1,21 @@
 <reference types="cypress" />
 
-// Assessment
+const username = 'taylor@wilsonlomax.com';
+const password = 'Otu$SDET123'
+
+// Accessing Assessment Screen
+
+describe('Accessing Assessment Screen', () => {
+  
+before(() => {
+  cy.login(username, password);
+
+})
+
+it('Login Successful', () => {
+  cy.find('.user-profile__user-name').should('equal', 'Taylor');
+})
+
+});
+
+// Taking an Assessment
