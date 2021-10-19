e98d9da0b0234dcdad7321dd2cb2c5224564513a
Taylor Lomax <taylor@wilsonlomax.com>
2021-10-18 21:30:42 -0500

    Merge branch 'main' of github.com:wilsonlomax/SDETFunctionalTest

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,5 +29,5 @@ Cypress.Commands.add('login', (email,password) => {
     cy.get('.login-form__section1__email').type(email);
     cy.get('.login-form__section1__password').type(password);
     cy.get('.btn-login').click();
-    cy.wait(1000);
+    cy.wait();
 })
