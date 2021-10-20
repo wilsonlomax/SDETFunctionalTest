4adcf8b4385d8df1ceb25326809408be6009f584
Taylor Lomax <taylor@wilsonlomax.com>
2021-10-20 08:11:11 -0500

    Added Comments to SQL Test

--- SQLTests/sqltest.sql
+++ SQLTests/sqltest.sql
@@ -1,9 +1,10 @@
-SQL queries
-a. Student, grade level
+/* SQL queries TEST */
+
+/* a. Student, grade level */
 
 select name, grade_id from user;
 
-b. Student, assessment name, and corresponding grade
+/* b. Student, assessment name, and corresponding grade */
 
 select user.name as Student, 
 assessment.title as Assessment_Name, 
@@ -13,12 +14,12 @@ Left Join assignment On user.user_id = assignment.user_id
 Left Join assessment On assignment.assessment_id = assessment.id
 Order By user.name;
 
-c. The number of assessments that the total available points are greater than 60
+/* c. The number of assessments that the total available points are greater than 60 */
 
 select count(*) from assessment
 where total_points > 60;
 
-2. Show how you would find where the value is null for allow_multiple_attempts
+/* 2. Show how you would find where the value is null for allow_multiple_attempts */
 
 select * from assessment
 where allow_multiple_attempts IS NULL;
