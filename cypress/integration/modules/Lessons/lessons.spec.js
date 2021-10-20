d836fecfbc23446a75d13f96692624185cb6fea8
Taylor Lomax <taylor@wilsonlomax.com>
2021-10-20 00:03:09 -0500

    Added Lesson Tests

--- cypress/integration/modules/Lessons/lessons.spec.js
+++ cypress/integration/modules/Lessons/lessons.spec.js
@@ -10,20 +10,42 @@ describe('Accessing Assessment Screen', () => {
   
   })
   
-  it('Access Lessons Page Page', () => {
+  it('Access Lessons Page', () => {
     cy.get('.ot-todo-list-item__heading').click();
     cy.get('.student-lesson__header__left__title').should('be.visible');
 
   });
 
-  it('7 Assignments, Links to Assignment, Completed Status', () => {
+  it('7 Assignments for User Found, Links to Assignment, Completed Status', () => {
     
     cy.get('.ot-lesson-card-header').should('have.length', 7);
     cy.get('.ot-lesson-card-header').first().should('be.visible').as('assignment');
     cy.get('@assignment').click();
     cy.contains('Go to' || 'Download');
     cy.contains('Completed');
+    cy.get('button[aria-label="Close"]').click();
   
   });
+
+  it('Completed Label Found when Assignment Card Opened and Assignment not Complete', () => {
+    cy.get('.ot-lesson-card-header').last().click();
+    cy.get('.ot-checkbox__label').contains('Complete');
+    cy.get('button[aria-label="Close"]').click();
+
+  });
+
+  it('Completed Label on Assignment Card after Checking Completed Checkbox', () => {
+    
+    cy.get('.ot-lesson-card-header').last().click().as('lastCard');
+    cy.get('.ot-checkbox__label').contains('Complete').as('Complete');
+    cy.get('@Complete').click();
+    cy.get('lesson-card').last().contains('Completed');
+    cy.get('.ot-lesson-card-header').last().click();
+    cy.get('@Complete').click();
+    cy.get('button[aria-label="Close"]').click();
+
+  });
+
+
   
 });
